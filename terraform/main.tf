3f0c48fcb98dc08d0e648e0475f070759616a499
Khushveer01 <130538249+Khushveer01@users.noreply.github.com>
2025-04-06 21:25:48 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "4fc59329-05df-497c-b332-226931637540"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-jenkins"
+  location = var.location
+}
+
+resource "azurerm_service_plan" "this" {
+  name                = "appserviceplan-jenkins"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  os_type             = "Linux"
+
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "this" {
+  name                = "Khushveer@1103"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  service_plan_id     = azurerm_service_plan.this.id  # Updated attribute
+
+  site_config {
+    always_on = true
+  }
+}
